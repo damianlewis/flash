f660e62a5d2ea78f7086cb6b971aa03efef054d6
Damian Lewis <damian@damianlewis.co.uk>
2014-12-23 17:05:18 +0000

    Fixed bug with class declaration on alert

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -2,7 +2,7 @@
     @if (Session::has('flash_notification.overlay'))
         @include('flash::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
     @else
-        <div class="alert {{ Session::get('flash_notification.level') }}" data-alert>
+        <div class="alert-box {{ Session::get('flash_notification.level') }}" data-alert>
             {{ Session::get('flash_notification.message') }}
             <a href="#" class="close">&times;</a>
         </div>
